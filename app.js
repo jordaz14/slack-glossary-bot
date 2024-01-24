93a575b48462fdd6c4f0c25415421bf57646bedf
Julian Ordaz <1julianordaz@gmail.com>
2024-01-23 23:00:13 -0500

    test 2

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const app = new App({
   appToken: process.env.SLACK_APP_KEY,
   customRoutes: [
     {
-      path: 'https://slack-glossary-bot.onrender.com',
+      path: 'https://slack-glossary-bot.onrender.com/slack/events',
       method: ['POST'],
       handler: (req, res) => {
         res.writeHead(200);
